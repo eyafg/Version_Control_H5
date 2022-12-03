8fd7f14b2a647cbae3ca41785adbc8e54f7619cd
Eya Fgaier <eya.fgaier@fau.de>
2022-12-03 15:12:58 +0100

    Python Packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+
+    name="snowflake",
+    version="1",
+    author="DSSS",
+    author_email="eya.fgaier@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy","turtle","random"]
+)
